c3a3507d4f8d0dc89c966055a74df661d93975d5
alamshafil <alamshafil@pm.me>
2023-01-17 18:08:26 -0500

    depends: Add libxcb_util_* to packages.mk and remove uneeded libX11 + zlib

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -2,9 +2,9 @@ packages:=boost openssl libevent zeromq
 native_packages := native_ccache
 
 qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf zlib
+qt_packages = qrencode protobuf
 
-qt_x86_64_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
+qt_x86_64_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
